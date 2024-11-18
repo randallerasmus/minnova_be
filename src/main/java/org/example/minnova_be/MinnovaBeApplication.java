723be1be4d19a58d7a0c6493a2eb02634b52669a
randall.erasmus <randall.erasmus@interfront.co.za>
2024-11-18 14:51:28 +0200

    Initial commit

---
+++ src/main/java/org/example/minnova_be/MinnovaBeApplication.java
@@ -0,0 +1,13 @@
+package org.example.minnova_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinnovaBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MinnovaBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/minnova_be/MinnovaBeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.minnova_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinnovaBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
